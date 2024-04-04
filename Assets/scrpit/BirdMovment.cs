fb7b177d75aff6cd832c4bc3bff5588582d252f8
tmann12 <terrancem29@nycstudents.net>
2024-04-04 09:27:15 -0400

    Initial commit

---
+++ Assets/scrpit/BirdMovment.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdMovment : MonoBehaviour
+{
+    Rigidbody2D bird;
+    // Start is called before the first frame update
+    void Start() 
+    {
+       bird= GetComponent<Rigidbody2D>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+
+            bird.AddForce(new  Vector2 (0,1) * 200);
+        }
+
+    }
+}
