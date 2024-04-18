9fb7ae297b4b3fba66de5bb2ca35892a098d30bf
tmann12 <terrancem29@nycstudents.net>
2024-04-18 09:27:21 -0400

    start working on Spawn manger

---
+++ Assets/scrpit/SpawnManger.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManger : MonoBehaviour
+{
+    public GameObject pipePrefab;
+    float lowHeight = -5.45f;
+    float highHeight = -1.85f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
